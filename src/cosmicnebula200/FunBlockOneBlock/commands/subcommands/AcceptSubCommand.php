88cd5967ac6dc653703c7dea177d3d194246c957
cosmicnebula200 <cosmicnebula25@gmail.com>
2021-12-10 22:41:32 +0530

    accept my invite ok

--- src/cosmicnebula200/FunBlockOneBlock/commands/subcommands/AcceptSubCommand.php
+++ src/cosmicnebula200/FunBlockOneBlock/commands/subcommands/AcceptSubCommand.php
@@ -5,7 +5,9 @@ namespace cosmicnebula200\FunBlockOneBlock\commands\subcommands;
 use CortexPE\Commando\args\RawStringArgument;
 use CortexPE\Commando\BaseSubCommand;
 use cosmicnebula200\FunBlockOneBlock\FunBlockOneBlock;
+use cosmicnebula200\FunBlockOneBlock\invites\Invite;
 use pocketmine\command\CommandSender;
+use pocketmine\player\Player;
 
 class AcceptSubCommand extends BaseSubCommand
 {
@@ -19,6 +21,25 @@ class AcceptSubCommand extends BaseSubCommand
     public function onRun(CommandSender $sender, string $aliasUsed, array $args): void
     {
         $invite = FunBlockOneBlock::getInstance()->getInviteManager()->getPlayerInvites($args['name']);
+        if ($invite instanceof Invite)
+            return;
+
+        if (!$invite->handleInvite())
+            return;
+
+        $player = FunBlockOneBlock::getInstance()->getPlayerManager()->getPlayerByPrefix($sender->getName());
+        $inviter = FunBlockOneBlock::getInstance()->getPlayerManager()->getPlayer($invite->getInviter());
+        $player->setOneBlock($inviter->getOneBlock());
+        $oneBlock = FunBlockOneBlock::getInstance()->getOneBlockManager()->getOneBlock($player->getOneBlock());
+        foreach ($oneBlock->getMembers() as $member)
+        {
+            $mbr = FunBlockOneBlock::getInstance()->getServer()->getPlayerByPrefix($member);
+            if ($mbr instanceof Player)
+                $mbr->sendMessage(FunBlockOneBlock::getInstance()->getMessages()->getMessage('invite-accepted', [
+                    "{PLAYER}" => $sender->getName()
+                ]));
+        }
+
     }
 
 }
